7b331c3a42807aceed575a9eb8bca9bcd92ec7f6
David Collins <dcollins@nygenome.org>
2024-06-13 08:58:27 -0400

    Quicksave barqs.trim

--- src/barqs.py
+++ src/barqs.py
@@ -1,4 +1,6 @@
-from cutadapt.adapters import
+from cutadapt.adapters import AnywhereAdapter
+
+import fasta
 import fastq
 
 
@@ -17,6 +19,36 @@ def extract(
     quality_scores = quality_scores[barcode_length + umi_length :]
 
     header = f"{header} {barcode}:{umi}"
+
+    return (
+        header,
+        seq,
+        quality_scores,
+    )
+
+
+def trim(record: fastq.FASTQRecord, features: fasta.FASTAish) -> fastq.FASTQRecord:
+
+    header, seq, quality_scores = record
+
+    feature_matches = {
+        feature_name: AnywhereAdapter(feature).match_to(seq)
+        for feature_name, feature in features
+    }
+
+    if any(feature_matches):
+        match_name, top_match = max(
+            feature_matches.items(), key=lambda x: x[1].score if x else 0
+        )
+
+        header = f"{header} {match_name}"
+
+        feature_start = top_match.rstart
+        feature_stop = top_match.rstop
+
+        seq = seq[feature_start:feature_stop]
+
+        quality_scores = quality_scores[feature_start:feature_stop]
 
     return (
         header,
