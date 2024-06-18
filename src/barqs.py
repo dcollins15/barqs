cbd0b0816d993520334685acd4ee82319e6293d4
David Collins <dcollins@nygenome.org>
2024-06-17 23:18:20 -0400

    Quicksave

--- src/barqs.py
+++ src/barqs.py
@@ -7,19 +7,19 @@ import fastq
 
 
 def extract(
-    read1: fastq.FASTQRecord,
+    read: fastq.FASTQRecord,
     barcode_length: int,
     umi_length: int,
-    read2: Optional[fastq.FASTQRecord] = None,
+    paired_read: Optional[fastq.FASTQRecord] = None,
 ) -> fastq.FASTQRecord:
 
-    header, seq, quality_scores = read1
+    header, seq, quality_scores = read
 
     barcode = seq[:barcode_length]
     umi = seq[barcode_length : barcode_length + umi_length]
 
-    if read2:
-        header, seq, quality_scores = read2
+    if paired_read:
+        header, seq, quality_scores = paired_read
     else:
         seq = seq[barcode_length + umi_length :]
         quality_scores = quality_scores[barcode_length + umi_length :]
@@ -35,27 +35,25 @@ def extract(
 
 def trim_by_regex(
     read: fastq.FASTQRecord, 
-    features: fasta.FASTAish,
+    feature_lookup: Mapping[str, str],
     tolerance = 3,
 ) -> fastq.FASTQRecord:
     
     header, seq, quality_scores = read
 
-    fuzzy_pattern = f"{{e<={tolerance}}}"
-    feature_matches = {
-        feature_name: match
-        for feature_name, feature_seq in features
-        if (match := regex.search(f"{feature_seq}{fuzzy_pattern}", seq))
+    match_lookup = {
+        match: feature
+        for feature in feature_lookup.keys()
+        if (match := regex.search(f"{feature}{{e<={tolerance}}}", seq))
     }
+    if any(match_lookup):
+        match = min(match_lookup.keys(), key=lambda x: x.fuzzy_counts)
+        feature = match_lookup[match]
+        feature_name = feature_lookup[feature]
 
-    if any(feature_matches):
-        match_name, top_match = min(
-            feature_matches.items(), key=lambda x: sum(x[1].fuzzy_counts)
-        )
-
-        header = f"{header} {match_name}"
-        seq = top_match.group()
-        quality_scores = quality_scores[top_match.start(): top_match.end()]
+        header = f"{header} {feature_name}"
+        seq = match.group()
+        quality_scores = quality_scores[match.start(): match.end()]
 
     return (
         header,
@@ -66,7 +64,7 @@ def trim_by_regex(
 
 def trim_by_index(
     read: fastq.FASTQRecord, 
-    features_lookup: Mapping[str, str],
+    feature_lookup: Mapping[str, str],
     region: tuple[str, str]
 ) -> fastq.FASTQRecord:
 
@@ -76,7 +74,7 @@ def trim_by_index(
     seq = seq[start: end]
     quality_scores = quality_scores[start: end]
 
-    feature_name = features_lookup.get(seq)
+    feature_name = feature_lookup.get(seq)
     if feature_name:
         header = f"{header} {feature_name}"
 
@@ -107,7 +105,8 @@ def quantify(
         barcode, umi = get_barcodes(header)
 
         feature_matches = [
-            feature_name for feature_name, _ in features if feature_name in header
+            feature_name 
+            for feature_name, _ in features if feature_name in header
         ]
 
         if feature_matches:
